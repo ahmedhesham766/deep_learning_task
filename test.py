03cf2a3fa221d87f316895b61b4e1e293aef8f49
ali01 <ali.01119544719@gmail.com>
2022-11-02 02:32:39 +0200

    finished the preprocessing of the data.

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-
 import pandas as pd
-
-from sklearn.model_selection import train_test_split
-
-
-
-
 
 dataset = pd.read_csv('penguins.csv')
 # preprocessing
@@ -14,62 +7,27 @@ dataset[dataset.columns[0]] = pd.Categorical(
     categories=['Adelie', 'Gentoo', 'Chinstrap']
 ).codes
 
-types = [dataset[0:50].copy(), dataset[50:100].copy(), dataset[100:150].copy()]
+dataset[dataset.columns[1]].fillna(inplace=True, value=dataset[dataset.columns[1]].mean())
+dataset[dataset.columns[2]].fillna(inplace=True, value=dataset[dataset.columns[2]].mean())
+dataset[dataset.columns[3]].fillna(inplace=True, value=dataset[dataset.columns[3]].mean())
+dataset[dataset.columns[5]].fillna(inplace=True, value=dataset[dataset.columns[5]].mean())
+# gender below the only nominal feature
+dataset[dataset.columns[4]].fillna(inplace=True, value='unknown')
+dataset[dataset.columns[4]] = pd.Categorical(
+    dataset[dataset.columns[4]],
+    categories=['unknown', 'male', 'female']
+).codes
 
-del dataset
+Train_Data = pd.concat([dataset[0:30], dataset[50:80], dataset[100:130]])
+Test_Data = pd.concat([dataset[30:50], dataset[80:100], dataset[130:150]])
 
-X_Train = []
-Y_Train = []
-X_Test = []
-Y_Test = []
+Train_Data = Train_Data.sample(frac=1, random_state=1).reset_index(drop=True)
+Test_Data = Test_Data.sample(frac=1, random_state=1).reset_index(drop=True)
 
-# preprocessing each one of the species alone and generating train and test data for every specie
-for data in types:
-    data[data.columns[1]].fillna(inplace=True, value=data[data.columns[1]].mean())
-    data[data.columns[2]].fillna(inplace=True, value=data[data.columns[2]].mean())
-    data[data.columns[3]].fillna(inplace=True, value=data[data.columns[3]].mean())
-    data[data.columns[5]].fillna(inplace=True, value=data[data.columns[5]].mean())
-    # gender below the only nominal feature
-    data[data.columns[4]].fillna(inplace=True, value='unknown')
-    data[data.columns[4]] = pd.Categorical(
-        data[data.columns[4]],
-        categories=['unknown', 'male', 'female']
-    ).codes
-    x = data[data.columns[1:6]]
-    y = data[data.columns[0]]
-    
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
-    X_Train.append(x_train)
-    X_Test.append(x_test)
-    Y_Train.append(y_train)
-    Y_Test.append(y_test)
+X_Train = Train_Data[Train_Data.columns[1:6]]
+X_Test = Test_Data[Test_Data.columns[1:6]]
+Y_Train = Train_Data[Train_Data.columns[0]]
+Y_Test = Test_Data[Test_Data.columns[0]]
 
 
 
-# concatenating the train and test data together
-X_Train = pd.concat([X_Train[0], X_Train[1], X_Train[2]])
-X_Test = pd.concat([X_Test[0], X_Test[1], X_Test[2]])
-Y_Train = pd.concat([Y_Train[0], Y_Train[1], Y_Train[2]])
-Y_Test = pd.concat([Y_Test[0], Y_Test[1], Y_Test[2]])
-
-# randomly shuffling the train and test data
-X_Train = X_Train.sample(frac=1, random_state=1).reset_index()
-X_Test = X_Test.sample(frac=1, random_state=1).reset_index()
-Y_Train = Y_Train.sample(frac=1, random_state=1).reset_index()
-Y_Test = Y_Test.sample(frac=1, random_state=1).reset_index()
-
-
-
-# num_training_class = int(len(dataset) / 3)
-# train_len = int(0.6 * num_training_class)
-# # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.6, random_state=0)
-#
-# num_training = X_train.shape[0]  # number of training data
-# num_testing = X_test.shape[0]  # number of testing data
-# epochs = 200  # number of epochs to iterate
-# features = X_train.shape[1]  # number of input neurons
-# eta = 0.001  # learning rate
-# P = Perceptron(features=features, epochs=epochs, x_train_data=X_train, x_test_data=X_test, y_train_data=Y_train,
-#                y_test_data=Y_test, eta=eta, num_training=num_training, num_testing=num_testing)
-# #
-# P.fit()
