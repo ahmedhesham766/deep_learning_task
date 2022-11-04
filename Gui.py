e7ea69f48dbdaa873d6be50d06ab3e4fe908d5dd
MaherAhmed0 <maher7ahmed985@gmail.com>
2022-11-04 15:24:50 +0200

    layout edit and simple modifications

--- Gui.py
+++ Gui.py
@@ -1,52 +1,126 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
+import model
+from model import Features
+from model import Species
+from model import Perceptron
+from model import preprocess
+
+
+def feature_1_changed(event):
+    features[0] = Features(feature1_cmb['values'].index(feature1_cmb.get()) + 1)
+
+
+def feature_2_changed(event):
+    features[1] = Features(feature2_cmb['values'].index(feature2_cmb.get()) + 1)
+
+
+def class_1_changed(event):
+    goals[0] = Species(goal1_cmb['values'].index(goal1_cmb.get()))
+
+
+def class_2_changed(event):
+    goals[1] = Species(goal2_cmb['values'].index(goal2_cmb.get()))
+
+
+def get_eta():
+    eta = float(eta_txt.get())
+    return eta
+
+
+def get_epochs():
+    epochs = int(epoch_txt.get())
+    return epochs
+
+
+def func():
+    x_train, y_train, x_test, y_test = preprocess(features=features, goals=goals, dataset=model.dataset)
+
+    per = Perceptron(features=features,
+                     goals=goals,
+                     x_train_data=x_train,
+                     x_test_data=x_test,
+                     y_train_data=y_train,
+                     y_test_data=y_test,
+                     eta=get_eta(),
+                     epochs=get_epochs(),
+                     with_bias=bias_checkbox_var.get())
+    per.train()
+    per.test()
+    per.plot()
+
+
 gui = Tk()
 gui.title('Task_1 GUI')
-gui.geometry('500x700+550+70')
+gui.geometry('500x720+550+60')
 
 Title = Label(gui, text="User Input", fg='black', font=("Times New Roman", 20))
 Title.place(x=180, y=30)
 
-lbl1 = Label(gui, text="Select first feature", fg='black', font=("Times New Roman", 14))
-lbl1.place(x=50, y=130)
-cmb1 = ttk.Combobox(gui, width=27)
-cmb1['values'] = ('bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'gender', 'body_mass_g')
-cmb1.place(x=290, y=130)
+feature1_label = Label(gui, text="Select first feature", fg='black', font=("Times New Roman", 14))
+feature1_label.place(x=50, y=130)
+selected_feature1 = tk.StringVar()
+feature1_cmb = ttk.Combobox(gui, textvariable=selected_feature1, width=27)
+feature1_cmb['values']: list = [e.name for e in Features]
+feature1_cmb.place(x=290, y=130)
 
-lbl2 = Label(gui, text="Select second feature", fg='black', font=("Times New Roman", 14))
-lbl2.place(x=50, y=200)
-cmb2 = ttk.Combobox(gui, width=27)
-cmb2['values'] = ('bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'gender', 'body_mass_g')
-cmb2.place(x=290, y=200)
+features: list = [None] * 2
+goals: list = [None] * 2
 
-lbl3 = Label(gui, text="Select 2 classes combination", fg='black', font=("Times New Roman", 14))
-lbl3.place(x=50, y=270)
-cmb3 = ttk.Combobox(gui, width=27)
-cmb3['values'] = ('Adelie && Gentoo', 'Adelie && Chinstrap', 'Gentoo && Chinstrap')
-cmb3.place(x=290, y=270)
+feature1_cmb.bind('<<ComboboxSelected>>', feature_1_changed)
 
-lbl4 = Label(gui, text="Enter learning rate", fg='black', font=("Times New Roman", 14))
-lbl4.place(x=50, y=340)
-txt1 = Entry(gui, bg='white', fg='black', bd=9, width=27)
-txt1.place(x=290, y=340)
+feature2_label = Label(gui, text="Select second feature", fg='black', font=("Times New Roman", 14))
+feature2_label.place(x=50, y=200)
+selected_feature2 = tk.StringVar()
+feature2_cmb = ttk.Combobox(gui, textvariable=selected_feature2, width=27)
+feature2_cmb['values'] = [e.name for e in Features]
+feature2_cmb.place(x=290, y=200)
+feature2_cmb.bind('<<ComboboxSelected>>', feature_2_changed)
 
-lbl5 = Label(gui, text="Enter number of epochs", fg='black', font=("Times New Roman", 14))
-lbl5.place(x=50, y=410)
-txt2 = Entry(gui, bg='white', fg='black', bd=9, width=27)
-txt2.place(x=290, y=410)
+goal1_lbl = Label(gui, text="Select first class", fg='black', font=("Times New Roman", 14))
+goal1_lbl.place(x=50, y=270)
+selected_class1 = tk.StringVar()
+goal1_cmb = ttk.Combobox(gui, textvariable=selected_class1, width=27)
+goal1_cmb['values'] = [e.name for e in Species]
+goal1_cmb.place(x=290, y=270)
+goal1_cmb.bind('<<ComboboxSelected>>', class_1_changed)
 
-lbl6 = Label(gui, text="Add bias or not", fg='black', font=("Times New Roman", 14))
-lbl6.place(x=50, y=480)
-cb = Checkbutton(gui, text="Bias")
-cb.place(x=360, y=485)
+goal2_lbl = Label(gui, text="Select second class", fg='black', font=("Times New Roman", 14))
+goal2_lbl.place(x=50, y=340)
+selected_class2 = tk.StringVar()
+goal2_cmb = ttk.Combobox(gui, textvariable=selected_class2, width=27)
+goal2_cmb['values'] = [e.name for e in Species]
+goal2_cmb.place(x=290, y=340)
+goal2_cmb.bind('<<ComboboxSelected>>', class_2_changed)
 
-btn = Button(gui, text="Run", fg='black', width=15, font=("Times New Roman", 14))
-btn.place(x=180, y=580)
+eta_lbl = Label(gui, text="Enter learning rate", fg='black', font=("Times New Roman", 14))
+eta_lbl.place(x=50, y=410)
+eta_txt = Entry(gui, bg='white', fg='black', bd=9, width=27)
+eta_txt.place(x=290, y=410)
+
+epoch_lbl = Label(gui, text="Enter number of epochs", fg='black', font=("Times New Roman", 14))
+epoch_lbl.place(x=50, y=480)
+epoch_txt = Entry(gui, bg='white', fg='black', bd=9, width=27)
+epoch_txt.place(x=290, y=480)
+
+bias_lbl = Label(gui, text="Add bias or not", fg='black', font=("Times New Roman", 14))
+bias_lbl.place(x=50, y=550)
+bias_checkbox_var = tk.StringVar()
+bias_cb = ttk.Checkbutton(gui,
+                          text='Bias',
+                          variable=bias_checkbox_var,
+                          onvalue=True,
+                          offvalue=False)
+bias_cb.place(x=360, y=555)
+
+btn = Button(gui,
+             text="Run",
+             fg='black',
+             width=15,
+             font=("Times New Roman", 14),
+             command=func)
+btn.place(x=180, y=620)
 
 gui.mainloop()
-
-
-
-
-
---
+++ model.py
@@ -0,0 +1,208 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from enum import Enum
+
+dataset = pd.read_csv('penguins.csv')
+
+dataset[dataset.columns[0]] = pd.Categorical(dataset[dataset.columns[0]],
+                                             categories=['Adelie', 'Gentoo', 'Chinstrap']).codes
+
+dataset[dataset.columns[4]].fillna(inplace=True, value='unknown')
+dataset[dataset.columns[4]] = pd.Categorical(dataset[dataset.columns[4]],
+                                             categories=['unknown', 'male', 'female']).codes
+
+
+class Species(Enum):
+    Adelie = 0
+    Gentoo = 1
+    Chinstrap = 2
+
+
+class Features(Enum):
+    bill_length_mm = 1
+    bill_depth_mm = 2
+    flipper_length_mm = 3
+    gender = 4
+    body_mass_g = 5
+
+
+############## Preprocessing ##############
+def preprocess(features, goals, dataset: pd.DataFrame):
+    # print(features[0])
+    # print(features[1])
+    # print(goals[0])
+    # print(goals[1])
+    data = {'species': dataset['species'].values,
+            features[0].name: dataset[features[0].name].values,
+            features[1].name: dataset[features[1].name].values}
+    data = pd.DataFrame(data=data)
+
+    y1 = goals[0].value * 50  # the start index of y1
+    y2 = goals[1].value * 50  # the start index of y1
+    data = pd.concat([data[y1:y1 + 50], data[y2:y2 + 50]])
+
+    del y1, y2
+
+    columns = data.columns.values
+    indices = []
+
+    for i in range(len(columns)):
+        if columns[i] not in [columns[0], features[0].name, features[1].name]:
+            indices.append(i)
+
+    columns = np.delete(columns, indices)
+    data = data[columns]
+
+    del columns, indices
+
+    type1: pd.DataFrame = data[0:50].copy()
+    type2: pd.DataFrame = data[50:100].copy()
+
+    del data
+    # print(type1, '\n-----------------------------\n', type2)
+
+    type1[type1.columns[1]].fillna(inplace=True, value=type1[type1.columns[1]].mean())
+    type1[type1.columns[2]].fillna(inplace=True, value=type1[type1.columns[2]].mean())
+    type1['species'] = 1
+
+    type2[type2.columns[1]].fillna(inplace=True, value=type2[type2.columns[1]].mean())
+    type2[type2.columns[2]].fillna(inplace=True, value=type2[type2.columns[2]].mean())
+    type2['species'] = -1
+
+    # print(type1, '\n--------------\n', type2)
+
+    train_data = pd.concat([type1[0:30], type2[0:30]])
+    test_data = pd.concat([type1[30:50], type2[30:50]])
+
+    del type1, type2
+    # print(train_data, '\n------------\n', test_data)
+
+    train_data = train_data.sample(frac=1, random_state=1).reset_index(drop=True)
+    test_data = test_data.sample(frac=1, random_state=1).reset_index(drop=True)
+
+    # print(train_data, '\n------------\n', test_data)
+
+    y_train: pd.DataFrame = train_data[train_data.columns[0]]
+    x_train: pd.DataFrame = train_data[train_data.columns[1:3]]
+    y_test: pd.DataFrame = test_data[test_data.columns[0]]
+    x_test: pd.DataFrame = test_data[test_data.columns[1:3]]
+
+    del train_data, test_data
+
+    return x_train, y_train, x_test, y_test
+
+
+class Perceptron:
+
+    # initialize perceptron without bias
+    def __init__(self, features, goals, epochs, x_train_data: pd.DataFrame, x_test_data: pd.DataFrame,
+                 y_train_data: pd.DataFrame, y_test_data: pd.DataFrame, eta, with_bias):
+
+        self.features = features  # input features
+        self.goals = goals  # input classes
+        self.epochs = epochs  # number of epochs
+        self.eta = eta  # learning rate
+        self.weight = np.random.rand(len(features) + 1)
+        x0 = pd.DataFrame(np.zeros(len(x_train_data)), columns=['bias'])
+        self.x_train_data = pd.concat([x0, x_train_data], axis=1)
+        x0 = pd.DataFrame(np.zeros(len(x_test_data)), columns=['bias'])
+        self.x_test_data = pd.concat([x0, x_test_data], axis=1)
+        del x0
+
+        if with_bias:
+            self.x_train_data['bias'] = 1
+            self.x_test_data['bias'] = 1
+
+        self.y_train_data = y_train_data
+        self.y_test_data = y_test_data
+
+    def activation_func_signum(self, x):
+        y = np.transpose(self.weight).dot(x)
+
+        if y < 0:
+            return -1
+        else:
+            return 1
+
+    def train(self):  # learn through the number of training samples
+        for j in range(self.epochs):
+            for i in range(len(self.x_train_data)):
+                # fetch data
+                x = self.x_train_data.values[i]
+                y = self.activation_func_signum(x)
+                t = self.y_train_data.values[i]
+
+                # calculate difference
+                loss = t - y
+
+                # new weight = old weight + (np.transpose(x).dot(loss).dot(self.eta))
+                self.weight = self.weight + np.transpose(x) * loss * self.eta
+            # print('epoch ' + str(j) + ', fails = ' + str(fails))
+            # # calculate mean squared error for each epoch
+            # print('epoch ' + str(j) + ': MSE = ' + str(fails / len(self.x_train_data)))
+
+        # training_accuracy = 100 - ((self.lost[self.num_training - 1] / self.num_training) * 100)
+        # print(f'Total samples trained: {self.num_training}')
+        # print(f'Training accuracy: {training_accuracy}%')
+        # print(f'Total epochs: {self.epochs}')
+
+    def plot(self):
+        c1 = pd.DataFrame(columns=[self.x_train_data.columns])
+        c2 = pd.DataFrame(columns=[self.x_train_data.columns])
+        min_x, max_x = float('inf'), float('-inf')
+
+        for i in range(len(self.y_train_data)):
+            x = self.x_train_data.iloc[i]
+            if x[1] < min_x:
+                min_x = x[1]
+            if x[1] > max_x:
+                max_x = x[1]
+            if self.y_train_data.values[i] == 1:
+                c1.loc[len(c1)] = [x[0], x[1], x[2]]
+            elif self.y_train_data.values[i] == -1:
+                c2.loc[len(c2)] = [x[0], x[1], x[2]]
+            else:
+                print('false')
+
+        plt.figure('fig')
+        plt.scatter(c1[c1.columns[1]], c1[c1.columns[2]], color='grey')
+        plt.scatter(c2[c2.columns[1]], c2[c2.columns[2]], color='orange')
+        plt.xlabel(c1.columns.values[1])
+        plt.ylabel(c1.columns.values[2])
+
+        # w0 + w1 * x1 + w2 * x2 = 0 , x1 is x, x2 is y
+        # x2 = (- w0 - w1 * 13) / w2 -> x1 = 13
+
+        w0, w1, w2 = self.weight[0], self.weight[1], self.weight[2]
+        x = [min_x, max_x]
+        y = [(-w0 - w1 * min_x) / w2, (-w0 - w1 * max_x) / w2]
+
+        plt.plot(x, y, marker='o', color='purple')
+        # plt.plot()
+        plt.show()
+
+    def test(self):  # predict and calculate testing accuracy
+        length = len(self.x_test_data)
+        mse = 0
+        accuracy = 0
+        for i in range(length):
+            # fetch data
+            x = self.x_test_data.values[i]
+            # activation function to calc y^
+            y = self.activation_func_signum(x)
+            # fetch the real target
+            t = self.y_test_data.values[i]
+
+            if t == y:
+                accuracy += 1
+            else:
+                mse += np.square(t - y)
+
+        # calculate testing accuracy
+        mse /= length
+        accuracy = (accuracy / length) * 100
+
+        print(f'Testing MSE: {mse}')
+        print(f'Testing accuracy: {accuracy:.2f}%')
+        print('--------------------------------')
